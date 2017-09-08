35a298d6a82055fb608fde11c22b37a2bca87c84
Erik Dubbelboer <erik@dubbelboer.com>
2017-09-08 23:59:24 +0200

    Remove delegate frontend
    Using delegate doesn't support return values which we require to meet
    the ERC 20 standard. For now we'll just change Frontend addresses each
    time we want to upgrade the contract.

--- deptoken.sol
+++ deptoken.sol
@@ -29,140 +29,9 @@ interface ERC20Interface {
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
-contract Frontend {
+contract Frontend is ERC20Interface {
     address private owner;
-    address private backend;
-
-    function Frontend(address _backend) {
-        backend = _backend;
-        owner = msg.sender;
-    }
-
-    function getOwner() constant returns (address) {
-        return owner;
-    }
-
-    function setOwner(address _owner) external {
-        if (msg.sender != owner) {
-            throw;
-        }
-
-        owner = _owner;
-    }
-
-    function getBackend() constant returns (address) {
-        return backend;
-    }
-
-    function setBackend(address _backend) external {
-        if (msg.sender != owner) {
-            throw;
-        }
-        backend = _backend;
-    }
-
-    function() public {
-        if (!backend.delegatecall(msg.data)) {
-            throw;
-        }
-    }
-
-    function destruct(address _to) external {
-        if (msg.sender != owner) {
-            throw;
-        }
-
-        selfdestruct(_to);
-    }
-}
-
-contract Storage {
-    address private owner;
-    mapping(address => bool) private backends;
-
-    // Balances for each account
-    mapping(address => uint256) private balances;
-
-    // Owner of account approves the transfer of an amount to another account
-    mapping(address => mapping (address => uint256)) private allowed;
-
-    uint256 private minted;
-
-    function Storage() {
-        owner = msg.sender;
-    }
-
-    function getOwner() constant returns (address) {
-        return owner;
-    }
-
-    function setOwner(address _owner) external {
-        if (msg.sender != owner) {
-            throw;
-        }
-
-        owner = _owner;
-    }
-
-    function isBackendAllowed(address _backend) constant returns (bool) {
-        return backends[_backend];
-    }
-
-    function setBackend(address _backend, bool allowed) external {
-        if (msg.sender != owner) {
-            throw;
-        }
-        backends[_backend] = allowed;
-    }
-
-    function getBalance(address _owner) constant returns (uint256) {
-        return balances[_owner];
-    }
-
-    function setBalance(address _owner, uint256 _amount) external {
-        if (!backends[msg.sender]) {
-            throw;
-        }
-
-        balances[_owner] = _amount;
-    }
-
-    function getAllowance(address _owner, address _spender) constant returns (uint256) {
-        return allowed[_owner][_spender];
-    }
-
-    function setAllowance(address _owner, address _spender, uint256 _amount) external {
-        if (!backends[msg.sender]) {
-            throw;
-        }
-
-        allowed[_owner][_spender] = _amount;
-    }
-
-    function getMinted() constant returns (uint256) {
-        return minted;
-    }
-
-    function setMinted(uint256 _minted) external {
-        if (!backends[msg.sender]) {
-            throw;
-        }
-
-        minted = _minted;
-    }
-
-    function destruct(address _to) external {
-        if (msg.sender != owner) {
-            throw;
-        }
-
-        selfdestruct(_to);
-    }
-}
-
-contract Backend is ERC20Interface {
-    address private owner;
-    Storage private storage;
+    Backend private backend;
 
     string public constant symbol = "TTT";
     string public constant name = "Test Token";
@@ -176,9 +45,9 @@ contract Backend is ERC20Interface {
 
     event Destroy(address indexed _from, uint256 _value);
 
-    function Backend(address _storage) {
+    function Frontend(address _backend) {
         owner = msg.sender;
-        storage = Storage(_storage);
+        backend = Backend(_backend);
     }
 
     function getOwner() constant returns (address) {
@@ -196,19 +65,19 @@ contract Backend is ERC20Interface {
 
     // What is the balance of a particular account?
     function balanceOf(address _owner) constant returns (uint256) {
-        return storage.balanceOf(_owner);
+        return backend.balanceOf(_owner);
     }
 
     // Transfer the balance from owner's account to another account
     function transfer(address _to, uint256 _amount) external returns (bool) {
         uint256 cost = _transferCost;
         uint256 amountPlusCost = _amount + cost;
-        uint256 balanceFrom = storage.getBalance(msg.sender);
-        uint256 balanceTo = storage.getBalance(_to);
+        uint256 balanceFrom = backend.getBalance(msg.sender);
+        uint256 balanceTo = backend.getBalance(_to);
 
         if (balanceFrom >= amountPlusCost && balanceTo + _amount > balanceTo) {
-            storage.setBalance(msg.sender, balanceFrom - amountPlusCost);
-            storage.setBalance(_to, balanceTo + _amount);
+            backend.setBalance(msg.sender, balanceFrom - amountPlusCost);
+            backend.setBalance(_to, balanceTo + _amount);
             Transfer(msg.sender, _to, _amount);
             Destroy(_from, cost);
             return true;
@@ -220,14 +89,14 @@ contract Backend is ERC20Interface {
     function transferFrom(address _from, address _to, uint256 _amount) external returns (bool) {
         uint256 cost = _transferCost;
         uint256 amountPlusCost = _amount + cost;
-        uint256 balanceFrom = storage.getBalance(_from);
-        uint256 balanceTo = storage.getBalance(_to);
-        uint256 allowanceSpender = storage.getAllowance(_from, msg.sender);
+        uint256 balanceFrom = backend.getBalance(_from);
+        uint256 balanceTo = backend.getBalance(_to);
+        uint256 allowanceSpender = backend.getAllowance(_from, msg.sender);
 
         if (allowanceSpender > amountPlusCost && balanceFrom >= amountPlusCost && balanceTo + _amount > balanceTo) {
-            storage.setBalance(_from, balanceFrom - amountPlusCost);
-            storage.setBalance(_to, balanceTo + _amount);
-            storage.setAllowance(_from, msg.sender, allowanceSpender - amountPlusCost);
+            backend.setBalance(_from, balanceFrom - amountPlusCost);
+            backend.setBalance(_to, balanceTo + _amount);
+            backend.setAllowance(_from, msg.sender, allowanceSpender - amountPlusCost);
             Transfer(_from, _to, _amount);
             Destroy(_from, cost);
             return true;
@@ -237,12 +106,12 @@ contract Backend is ERC20Interface {
     }
 
     function approve(address _spender, uint256 _amount) external returns (bool) {
-        storage.setAllowance(msg.sender, _spender, _amount);
+        backend.setAllowance(msg.sender, _spender, _amount);
         return true;
     }
 
     function allowance(address _owner, address _spender) constant returns (uint256) {
-        return storage.getAllowance(_owner, _spender);
+        return backend.getAllowance(_owner, _spender);
     }
 
 
@@ -251,19 +120,19 @@ contract Backend is ERC20Interface {
             throw;
         }
         uint256 amount = msg.value / price;
-        uint256 minted = storage.getMinted();
+        uint256 minted = backend.getMinted();
         if (minted + amount < minted) {
             throw;
         }
         if (minted + amount > totalSupply) {
             throw;
         }
-        uint256 balance = storage.getBalance(msg.sender);
+        uint256 balance = backend.getBalance(msg.sender);
         if (balance + amount < balance) {
             throw;
         }
-        storage.setBalance(msg.sender, balance + amount);
-        storage.setMinted(minted + amount);
+        backend.setBalance(msg.sender, balance + amount);
+        backend.setMinted(minted + amount);
         Transfer(this, msg.sender, _amount);
         return amount;
     }
@@ -274,6 +143,90 @@ contract Backend is ERC20Interface {
         }
 
         _to.transfer(_amount);
+    }
+
+    function destruct(address _to) external {
+        if (msg.sender != owner) {
+            throw;
+        }
+
+        selfdestruct(_to);
+    }
+}
+
+contract Backend {
+    address private owner;
+    mapping(address => bool) private frontends;
+
+    // Balances for each account
+    mapping(address => uint256) private balances;
+
+    // Owner of account approves the transfer of an amount to another account
+    mapping(address => mapping (address => uint256)) private allowed;
+
+    uint256 private minted;
+
+    function Backend() {
+        owner = msg.sender;
+    }
+
+    function getOwner() constant returns (address) {
+        return owner;
+    }
+
+    function setOwner(address _owner) external {
+        if (msg.sender != owner) {
+            throw;
+        }
+
+        owner = _owner;
+    }
+
+    function isFrontendAllowed(address _frontend) constant returns (bool) {
+        return frontends[_frontend];
+    }
+
+    function setFrontend(address _frontend, bool allowed) external {
+        if (msg.sender != owner) {
+            throw;
+        }
+        frontends[_frontend] = allowed;
+    }
+
+    function getBalance(address _owner) constant returns (uint256) {
+        return balances[_owner];
+    }
+
+    function setBalance(address _owner, uint256 _amount) external {
+        if (!frontends[msg.sender]) {
+            throw;
+        }
+
+        balances[_owner] = _amount;
+    }
+
+    function getAllowance(address _owner, address _spender) constant returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    function setAllowance(address _owner, address _spender, uint256 _amount) external {
+        if (!frontends[msg.sender]) {
+            throw;
+        }
+
+        allowed[_owner][_spender] = _amount;
+    }
+
+    function getMinted() constant returns (uint256) {
+        return minted;
+    }
+
+    function setMinted(uint256 _minted) external {
+        if (!frontends[msg.sender]) {
+            throw;
+        }
+
+        minted = _minted;
     }
 
     function destruct(address _to) external {
