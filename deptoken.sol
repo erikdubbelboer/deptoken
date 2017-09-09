8ab1bd4593679b7b3076123bdba789d9deccd938
Erik Dubbelboer <erik@dubbelboer.com>
2017-09-09 22:31:11 +0000

    ERC20 interface contract that never changes
    Now we can upgrade our frontend contract while the ERC20 interface stays
    at the same address. This should make it easier for exchanges to
    implement this coin as they never have to worry about changes to the
    contract. While at the same time we can upgrade the actual Frontend
    contract to fix bugs or implement other functionality.

--- deptoken.sol
+++ deptoken.sol
@@ -1,54 +1,27 @@
 pragma solidity 0.4.8; 
 
-// ERC Token Standard #20 Interface
-// https://github.com/ethereum/EIPs/issues/20
-interface ERC20Interface {
-    // Get the total token supply
-    function totalSupply() constant returns (uint256 totalSupply);
-
-    // Get the account balance of another account with address _owner
-    function balanceOf(address _owner) constant returns (uint256 balance);
-
-    // Send _value amount of tokens to address _to
-    function transfer(address _to, uint256 _value) returns (bool success);
-
-    // Send _value amount of tokens from address _from to address _to
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-
-    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-    // If this function is called again it overwrites the current allowance with _value.
-    function approve(address _spender, uint256 _value) returns (bool success);
-
-    // Returns the amount which _spender is still allowed to withdraw from _owner
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-
-    // Triggered when tokens are transferred.
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    // Triggered whenever approve(address _spender, uint256 _value) is called.
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
-
-contract Frontend is ERC20Interface {
+contract Frontend {
     address private owner;
     Backend private backend;
+    ERC20Interface private erc20; // The address of our ERC20 interface contract.
 
-    string public constant symbol = "TTT";
-    string public constant name = "Test Token";
-    uint8 public constant decimals = 18;
-
-    uint256 public constant totalSupply = 1000000;
+    uint256 public constant total = 1000000; // Max number of tokens that can be in existence.
     uint256 public constant transferCost = 1;
 
     uint public constant mintAfterBlock = 1234;
     uint256 public constant price = 1000; // 1000 wei for one token.
 
     event Destroy(address indexed _from, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    function Frontend(address _backend) {
+
+    function Frontend(address _backend, address _erc20) {
         owner = msg.sender;
         backend = Backend(_backend);
+        erc20 = ERC20Interface(_erc20);
     }
+
 
     function getOwner() constant returns (address) {
         return owner;
@@ -62,6 +35,19 @@ contract Frontend is ERC20Interface {
         owner = _owner;
     }
 
+
+    function getBackend() constant returns (address) {
+        return backend;
+    }
+
+    function getERC20() constant returns (address) {
+        return erc20;
+    }
+
+
+    function totalSupply() constant returns (uint256) {
+        return total;
+    }
 
     // What is the balance of a particular account?
     function balanceOf(address _owner) constant returns (uint256) {
@@ -79,7 +65,31 @@ contract Frontend is ERC20Interface {
             backend.setBalance(msg.sender, balanceFrom - amountPlusCost);
             backend.setBalance(_to, balanceTo + _amount);
             Transfer(msg.sender, _to, _amount);
+            erc20.triggerTransfer(msg.sender, _to, _amount);
             Destroy(_from, cost);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Internal transfer function that can only be used by our ERC20 interface contract.
+    function internalTransfer(address _sender, address _to, uint256 _amount) external returns (bool) {
+        if (msg.sender != erc20) {
+            throw;
+        }
+
+        uint256 cost = _transferCost;
+        uint256 amountPlusCost = _amount + cost;
+        uint256 balanceFrom = backend.getBalance(_sender);
+        uint256 balanceTo = backend.getBalance(_to);
+
+        if (balanceFrom >= amountPlusCost && balanceTo + _amount > balanceTo) {
+            backend.setBalance(_sender, balanceFrom - amountPlusCost);
+            backend.setBalance(_to, balanceTo + _amount);
+            Transfer(_sender, _to, _amount);
+            erc20.triggerTransfer(msg.sender, _to, _amount);
+            Destroy(_sender, cost);
             return true;
         } else {
             return false;
@@ -98,6 +108,32 @@ contract Frontend is ERC20Interface {
             backend.setBalance(_to, balanceTo + _amount);
             backend.setAllowance(_from, msg.sender, allowanceSpender - amountPlusCost);
             Transfer(_from, _to, _amount);
+            erc20.triggerTransfer(msg.sender, _to, _amount);
+            Destroy(_from, cost);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Internal transferFrom function that can only be used by our ERC20 interface contract.
+    function internalTransferFrom(address _sender, address _from, address _to, uint256 _amount) external returns (bool) {
+        if (msg.sender != erc20) {
+            throw;
+        }
+
+        uint256 cost = _transferCost;
+        uint256 amountPlusCost = _amount + cost;
+        uint256 balanceFrom = backend.getBalance(_from);
+        uint256 balanceTo = backend.getBalance(_to);
+        uint256 allowanceSpender = backend.getAllowance(_from, _sender);
+
+        if (allowanceSpender > amountPlusCost && balanceFrom >= amountPlusCost && balanceTo + _amount > balanceTo) {
+            backend.setBalance(_from, balanceFrom - amountPlusCost);
+            backend.setBalance(_to, balanceTo + _amount);
+            backend.setAllowance(_from, _sender, allowanceSpender - amountPlusCost);
+            Transfer(_from, _to, _amount);
+            erc20.triggerTransfer(msg.sender, _to, _amount);
             Destroy(_from, cost);
             return true;
         } else {
@@ -107,6 +143,18 @@ contract Frontend is ERC20Interface {
 
     function approve(address _spender, uint256 _amount) external returns (bool) {
         backend.setAllowance(msg.sender, _spender, _amount);
+        Approval(msg.sender, _spender, _amount);
+        erc20.triggerApproval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    // Internal approve function that can only be used by our ERC20 interface contract.
+    function internalApprove(address _sender, address _spender, uint256 _amount) external returns (bool) {
+        if (msg.sender != erc20) {
+            throw;
+        }
+
+        backend.setAllowance(_sender, _spender, _amount);
         return true;
     }
 
@@ -124,7 +172,7 @@ contract Frontend is ERC20Interface {
         if (minted + amount < minted) {
             throw;
         }
-        if (minted + amount > totalSupply) {
+        if (minted + amount > total) {
             throw;
         }
         uint256 balance = backend.getBalance(msg.sender);
@@ -237,3 +285,88 @@ contract Backend {
         selfdestruct(_to);
     }
 }
+
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/issues/20
+contract ERC20Interface {
+    address private owner;
+    Frontend private frontend;
+
+    string public constant symbol = "TTT";
+    string public constant name = "Test Token";
+    uint8 public constant decimals = 18;
+
+    function ERC20Interface() {
+        owner = msg.sender;
+    }
+
+    function getOwner() constant returns (address) {
+        return owner;
+    }
+
+    function setOwner(address _owner) external {
+        if (msg.sender != owner) {
+            throw;
+        }
+
+        owner = _owner;
+    }
+
+    function getFrontend() constant returns (address) {
+        return frontend;
+    }
+
+    function setFrontend(address _frontend) external {
+        if (msg.sender != owner) {
+            throw;
+        }
+
+        frontend = Frontend(_frontend);
+    }
+
+    // Get the total token supply
+    function totalSupply() constant returns (uint256) {
+        return frontend.totalSupply();
+    }
+
+    // Get the account balance of another account with address _owner
+    function balanceOf(address _owner) constant returns (uint256) {
+        return frontend.balanceOf(_owner);
+    }
+
+    // Send _value amount of tokens to address _to
+    function transfer(address _to, uint256 _value) returns (bool) {
+        return frontend.internalTransfer(msg.sender, _to, _value);
+    }
+
+    // Send _value amount of tokens from address _from to address _to
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+        return frontend.internalTransferFrom(msg.sender, _from, _to, _value);
+    }
+
+    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+    // If this function is called again it overwrites the current allowance with _value.
+    function approve(address _spender, uint256 _value) returns (bool) {
+        return frontend.internalApprove(msg.sender, _spender, _value);
+    }
+
+    // Returns the amount which _spender is still allowed to withdraw from _owner
+    function allowance(address _owner, address _spender) constant returns (uint256) {
+        return frontend.allowance(_owner, _spender);
+    }
+
+    // Triggered when tokens are transferred.
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    // Triggered whenever approve(address _spender, uint256 _value) is called.
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    function triggerTransfer(address _from, address _to, uint256 _value) {
+        Transfer(_from, _to, _value);
+    }
+
+    function triggerApproval(address _owner, address _spender, uint256 _value) {
+        Approval(_owner, _spender, _value);
+    }
+}
+
